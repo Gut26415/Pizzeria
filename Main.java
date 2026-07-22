8f760ae00b6ce727f890b9e63075a47f27098b7b
Jonathan Gutierrez <gut26415@uvg.edu.gt>
2026-07-22 10:29:52 -0600

    Se crear usuario en Main

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main{
     public static void main(String[] args) {
-    
+        Usuario usuario1;
+
+        usuario1 = new Usuario("Jonathan", "10 calle zona 1");
+
+        System.out.println(usuario1.getId());
     }
 
 }
