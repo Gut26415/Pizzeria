a854b7f5c1975d07449b29887bc7f68b3f55fcce
Jonathan Gutierrez <gut26415@uvg.edu.gt>
2026-07-22 09:18:00 -0600

    Clases principales

---
+++ Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+    private int id;
+    private String nombre;
+    private int edad;
+    private String telefono;
+    private String direccion;
+    
+    public Usuario(int id, String nombre, int edad, String telefono, String direccion){
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.telefono = telefono;
+        this.direccion = direccion;
+
+        
+    }
+}
