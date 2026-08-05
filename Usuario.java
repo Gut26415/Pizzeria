aebbbc0da450db3b88c61f6392aea14d2dbf6e77
Jonathan Gutierrez <gut26415@uvg.edu.gt>
2026-08-05 17:32:52 -0600

    se cambia el constructor y se agregan funcionalidades

--- Usuario.java
+++ Usuario.java
@@ -1,41 +1,34 @@
+import java.time.LocalDate;
+
 public class Usuario {
-    private int id = 0;
+
+    private int id;
     private String nombre;
-    private int edad;
+    private LocalDate fechaNacimiento;
     private String telefono;
     private String direccion;
-    
-    public Usuario(int id, String nombre, int edad, String telefono, String direccion){
+
+    public Usuario(int id, String nombre, LocalDate fechaNacimiento, String telefono, String direccion) {
         this.id = id;
         this.nombre = nombre;
-        this.edad = edad;
+        this.fechaNacimiento = fechaNacimiento;
         this.telefono = telefono;
-        this.direccion = direccion; 
-    }
-
-    public Usuario(String nombre, String direccion){
-        id = id+1;
-        this.nombre = nombre;
         this.direccion = direccion;
     }
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public String getDireccion(){
-        return direccion;
-    }
-
-    public void setDireccion(String nuevaDireccion){
-        direccion = nuevaDireccion;
-    }
-
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombreNuevo){
-        nombre = nombreNuevo;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void actualizarDireccion(String direccion) {
+        this.direccion = direccion;
     }
 }
