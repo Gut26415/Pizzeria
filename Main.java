ee4249b47a36d84f197002fc004bdb6db9db3ba5
Jonathan Gutierrez <gut26415@uvg.edu.gt>
2026-08-05 17:34:48 -0600

    Se crea usuario con constructor completo

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
+import java.time.LocalDate;
+
 public class Main{
     public static void main(String[] args) {
         Usuario usuario1;
 
-        usuario1 = new Usuario("Jonathan", "10 calle zona 1");
+        usuario1 = new Usuario(1,
+            "Jonathan",
+            LocalDate.of(2007, 3, 06),
+            "1234-5678", 
+            "10 calle zona 1"
+        );
         
-
-        System.out.println(usuario1.getId());
+        System.out.println(usuario1);
     }
 
 }
