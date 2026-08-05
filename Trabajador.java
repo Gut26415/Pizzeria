9ab5e26a2d1f3f91596e0d327fc2347d2617b2a5
Jonathan Gutierrez <gut26415@uvg.edu.gt>
2026-08-05 17:33:46 -0600

    Se crea estructura para la clase trabajador

--- Trabajador.java
+++ Trabajador.java
@@ -1,3 +1,51 @@
-public class Trabajador {
-    
+import java.time.LocalDate;
+
+public class Trabajador extends Usuario {
+
+    public enum Puesto {
+        GERENTE,
+        PIZZERO,
+        RECEPTOR
+    }
+
+    private Puesto puesto;
+    private double salario;
+    private int codigo;
+
+    public Trabajador(int id, String nombre, LocalDate fechaNacimiento, String telefono, String direccion, Puesto puesto, double salario, int codigo) {
+        super(id, nombre, fechaNacimiento, telefono, direccion);
+        this.puesto = puesto;
+        this.salario = salario;
+        this.codigo = codigo;
+    }
+
+    public void prepararPizza(Pizza pizza) {}
+
+    public void tomarPedido() {}
+
+    public void ingresar() {}
+
+    public void salir() {}
+
+    public void entregarPizza(Pizza pizza) {}
+
+    public Puesto getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(Puesto puesto) {
+        this.puesto = puesto;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
 }
