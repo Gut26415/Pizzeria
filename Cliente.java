08f5cc8fd204cdc75c7e44d061413c1470b23e04
Sergio Gomez <sergiogomez@Sergios-MacBook-Pro.local>
2026-08-05 09:38:18 -0600

    feat: add getters and setters

--- Cliente.java
+++ Cliente.java
@@ -1,4 +1,63 @@
 public class Cliente {
+
+     enum TipoCliente{
+        TRADICIONAL, 
+        MUY_FRECUENTE,
+        VIP,
+        ESTUDIANTES
+    }
     
     private int puntos;
+   
+    private Pedido pedido;
+    private TipoCliente tipoCliente;
+
+    public Cliente(int puntos, TipoCliente tipoCliente, Pedido pedido){
+        this.puntos = puntos;
+        this.tipoCliente = tipoCliente;
+        this.pedido = pedido;
+    }
+
+    public Pedido hacerPedido(Pedido pedido){
+        return pedido;
+    }
+
+    public int verPuntos(){
+        return puntos;
+    }
+
+    public void acumularPuntos(int puntos){
+        System.out.println("Acumulando "+puntos+" puntos");
+    }
+
+    public void acumularPuntos(double compra, boolean promocion){
+        System.out.println("Acumulando puntos por la compra de "+compra+" con promocion: "+promocion);
+    }
+
+    //Getter Setter
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public TipoCliente getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(TipoCliente tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
 }
+
